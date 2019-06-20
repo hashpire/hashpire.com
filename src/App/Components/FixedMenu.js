51e1df1f7ac724c7ac8c4001668564b67f809d7b
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-06-20 22:14:05 +0800

    add fixed menu, always bottom footer

---
+++ src/App/Components/FixedMenu.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react';
+import Footer from './Footer';
+
+const FixedMenu = (props) => (
+  <div id='page'>
+    <div>
+      <Menu fixed='top' inverted id='navbar'>
+        <Container>
+          <Menu.Item as='a' header>
+            <Image size='small' src='/assets/images/logo.png' />
+          </Menu.Item>
+          <Menu.Item as='a'>Home</Menu.Item>
+
+          <Dropdown item simple text='Dropdown'>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Header>Header Item</Dropdown.Header>
+              <Dropdown.Item>
+                <i className='dropdown icon' />
+                <span className='text'>Submenu</span>
+                <Dropdown.Menu>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                  <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+      </Menu>
+      {props.children}
+    </div>
+    <Footer/>
+  </div>
+)
+
+export default FixedMenu;
--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import './Stylesheets/index.css';
 import MarkdownRenderer from '../Utils/MarkdownRenderer';
 import { Button } from 'semantic-ui-react';
-
-
+import FixedMenu from './Components/FixedMenu';
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react';
 class App extends React.Component {
   render() {
-    return <div><MarkdownRenderer url={'/assets/test.md'}/><Button>Click Here</Button></div>;
+    // <MarkdownRenderer url={'/assets/test.md'}/><Button>Click Here</Button>
+    return (
+      <FixedMenu>
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>Semantic UI React Fixed Template</Header>
+          <p>This is a basic fixed menu template using fixed size containers.</p>
+          <p>
+            A text container is used for the main container, which is useful for single column layouts.
+          </p>
+          <Image src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+          <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+        </Container>
+      </FixedMenu>
+    );
   }
 }
 
