60dd7b1b4c9cc9e2036112407022cde1c9b91a23
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-06-23 11:31:06 +0800

    add mobile responsive html tag

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IPFS from 'ipfs';
+// import IPFS from 'ipfs';
 import App from './App';
 
 
-const node = new IPFS()
+// const node = new IPFS()
 
-node.on('ready', async () => {
-  try {
-    const version = await node.version()
-    console.log('Version:', version.version)
-  } catch (error) {
-    console.error('Error', error)
-  }
-})
+// node.on('ready', async () => {
+//   try {
+//     const version = await node.version()
+//     console.log('Version:', version.version)
+//   } catch (error) {
+//     console.error('Error', error)
+//   }
+// })
 
 ReactDOM.render(
   <App/>,
