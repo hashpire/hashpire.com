79d216e5b9e9b4303b9987074e0dc165e5d29d0b
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-06-30 13:02:01 +0800

    add cards

--- src/App/Pages/Projects.js
+++ src/App/Pages/Projects.js
@@ -1,32 +1,67 @@
-import React from 'react';
-import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-} from 'semantic-ui-react';
-function Projects() {
-  return (
-          <Container text>
-            <Header as='h1'>Semantic UI React Fixed Template</Header>
-            <p>This is a basic fixed menu template using fixed size containers.</p>
-            <p>
-              A text container is used for the main container, which is useful for single column layouts.
-            </p>
-            <Image src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-            <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-          </Container>
-  );
+import React from "react";
+import { Container, Item, Label, Icon, Button, Card, Image } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
+class Projects extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      accounts: []
+    }
+  }
+
+  componentDidMount() {
+    var that = this;
+    fetch("/assets/data/universe.json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(universe) {
+        let accounts = universe.accounts;
+        let technologiesNode = universe.nodes[2];
+        console.log(technologiesNode);
+        let projects = technologiesNode.children.map((child) => {
+          return universe.nodes[child];
+        });
+        that.setState({
+          projects,
+          accounts
+        })
+      });
+  }
+
+  render() {
+    let { projects, accounts } = this.state;
+    return (
+      <Container text>
+        <Card.Group stackable>
+          {
+            projects.map((tech, index) => {
+              return (
+                <Card key={index} link as={Link} to={`/nodes/${tech.id}`}>
+                  <Image src='/assets/images/logo.png' wrapped ui={false} />
+                  <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Description>
+                      Daniel is a comedian living in Nashville.
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Icon name='user' />
+                    10 Friends
+                  </Card.Content>
+                </Card>
+              );
+            })
+          }
+
+        </Card.Group>
+
+
+      </Container>
+    );
+  }
 }
 
 export default Projects;
--- src/App/menuConfig.js
+++ src/App/menuConfig.js
@@ -11,20 +11,20 @@ const MenuConfig = [
     path: "/technologies",
     name: "Technologies"
   },
-  {
-    path: '/category',
-    name: 'Category',
-    sub: [
-      {
-        path: '/category/item1',
-        name: 'Item1'
-      },
-      {
-        path: '/category/item2',
-        name: 'Item2'
-      }
-    ]
-  }
+  // {
+  //   path: '/category',
+  //   name: 'Category',
+  //   sub: [
+  //     {
+  //       path: '/category/item1',
+  //       name: 'Item1'
+  //     },
+  //     {
+  //       path: '/category/item2',
+  //       name: 'Item2'
+  //     }
+  //   ]
+  // }
 ];
 
 export default MenuConfig;
