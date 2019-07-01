9ed903394a1da11648b5a2447714178f3753bb06
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-07-01 15:46:08 +0800

    add disqus comment

--- src/App/Pages/Post.js
+++ src/App/Pages/Post.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import NodeRenderer from '../Components/NodeRenderer';
+import Disqus from 'disqus-react';
+import { Container } from 'semantic-ui-react';
 
 class Post extends React.Component {
   render() {
     let nodeId = this.props.match.params.id;
+    console.log(this.props);
+    const disqusShortname = 'example';
+    const disqusConfig = {
+      url: this.props.match.url,
+      identifier: this.props.match.params.id,
+      title: 'Tata',
+    };
+
     return (
-      <Container>
-        <NodeRenderer nodeId={nodeId}/>
+      <Container text>
+        <NodeRenderer nodeId={nodeId} />
+        <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </Container>
     );
   }
