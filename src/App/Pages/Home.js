3e51b28e6201a1a3ec9582287b2cb760acafece6
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-07-01 16:28:25 +0800

    add twitter list to homepage

--- src/App/Pages/Home.js
+++ src/App/Pages/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import NodeRenderer from '../Components/NodeRenderer';
-import { Breadcrumb, Container } from 'semantic-ui-react';
+import { Breadcrumb, Container, Grid } from 'semantic-ui-react';
 
 class Home extends React.Component {
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
   render() {
     return (
-      <Container>
-        <NodeRenderer nodeId={1} />
-      </Container>
+      // <Container>
+          <Grid stackable centered>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <NodeRenderer nodeId={1} />
+              </Grid.Column>
+              <Grid.Column width={3} verticalAlign='middle'>
+                <a className="twitter-timeline" data-lang="th" data-width="400" data-height="500" data-theme="dark" href="https://twitter.com/anak0rn/lists/test?ref_src=twsrc%5Etfw">A Twitter List by anak0rn</a>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+      // </Container>
     )
   }
 }
